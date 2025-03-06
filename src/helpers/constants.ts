cbe90e083f93ac09014ab08434eac3ed542b94fc
Noel da Costa <noeldacosta@gmail.com>
2025-03-06 20:06:46 +0000

    Updated the module name

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,7 +1,7 @@
 // Class for general global variables.
 
-export const MODULE_ID = 'foundryvtt-final-fantasy-player-book';
-export const MODULE_TITLE = 'Final Fantasy XV Player Book';
+export const MODULE_ID = 'foundryvtt-final-fantasy-XIV-player-book';
+export const MODULE_TITLE = 'Final Fantasy XIV Player Book';
 export const LOG_PREFIX = 'FF15 Player Book |';
 export const MYSTERY_MAN = 'icons/svg/mystery-man.svg';
 export const NONE_ICON = 'icons/svg/cancel.svg';
