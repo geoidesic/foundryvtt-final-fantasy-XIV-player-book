0645f428a13c623e006081c2fe9d532f4a4b3423
Noel da Costa <noeldacosta@gmail.com>
2025-03-12 23:38:08 +0000

    Release 0.0.15

--- dist/index.js
+++ dist/index.js
@@ -14406,7 +14406,7 @@ Hooks.on("PopOut:close", (app) => {
     app.position.enabled = true;
   }
 });
-const version = "0.0.13";
+const version = "0.0.14";
 class WelcomeApplication extends SvelteApplication {
   /**
    * Default Application options
